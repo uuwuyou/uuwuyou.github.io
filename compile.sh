f71e495e044f91596664aadd43b17dae50c7e2f4
uuwuyou <uu.wy@alibaba-inc.com>
2017-01-11 12:27:38 +0800

    post a new article

--- compile.sh
+++ compile.sh
@@ -1,1 +1,8 @@
 hugo --theme=hyde --baseUrl="https://uuwuyou.github.io/" --buildDrafts
+mv public/.gitbak .git
+cd public
+git add -A
+git commit -am "post a new article"
+git push origin master
+mv .git .gitbak
+cd ..
---
+++ content/post/xinleileilei.md
@@ -0,0 +1,9 @@
++++
+date = "2017-01-11T12:24:49+08:00"
+draft = true
+title = "xinleileilei"
+
++++
+
+
+### xinleiiiiiiii
